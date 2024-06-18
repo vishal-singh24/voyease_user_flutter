62f3872b9ccb54695721eb1de33b3c625b115f63
Naman Tamrakar <namantam1@gmail.com>
2024-06-19 00:10:25 +0530

    Add map component

---
+++ lib/screens/app_map.dart
@@ -0,0 +1,32 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class AppMap extends StatefulWidget {
+  const AppMap({super.key});
+
+  @override
+  State<AppMap> createState() => AppMapState();
+}
+
+class AppMapState extends State<AppMap> {
+  final Completer<GoogleMapController> _controller =
+      Completer<GoogleMapController>();
+
+  static const CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return GoogleMap(
+      mapType: MapType.normal,
+      initialCameraPosition: _kGooglePlex,
+      onMapCreated: (GoogleMapController controller) {
+        _controller.complete(controller);
+      },
+    );
+  }
+}
